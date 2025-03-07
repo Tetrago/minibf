f01f587be14df81849f5175eb15c5be99ad820c6
James Geiss <jamestgeiss@gmail.com>
2025-03-06 19:43:30 -0500

    feat: shave off a line

--- bf.asm
+++ bf.asm
@@ -1,7 +1,7 @@
         global  _start
-next:   inc qword [data + 0x10]
+next:   inc r14
         xor rax, rax
-        mov rdi, [data]
+        mov rdi, r12
         lea rsi, [rsp - 1]
         mov rdx, 1
         syscall
@@ -9,33 +9,33 @@ next:   inc qword [data + 0x10]
         test rax, rax
         cmovnz rax, rdx
         ret
-_start: mov rdi, [rsp + 0x10]
+_start: xor r14, r14
+        mov rdi, [rsp + 0x10]
         mov rax, 2
         xor rsi, rsi
         xor rdx, rdx
         syscall
-        mov [data], rax
+        mov r12, rax
         sub rsp, 0x200
-        mov [data + 8], rsp
+        mov r13, rsp
         mov rcx, 0x200 / 8
         xor rax, rax
         mov rdi, rsp
         rep stosq
 loop:   call next
-        jmp [data + rax * 8 + 0x18]
-id:     mov rdx, [data + 8]
+        jmp [data + rax * 8]
+id:     mov rdx, r13
         sub rax, 44
         sub [rdx], al
         jmp loop
 lr:     sub rax, 61
-        add [data + 8], al
+        add r13, rax
         jmp loop
-start:  mov rax, [data + 8]
+start:  mov rax, r13
         mov al, [rax]
         test al, al
         jz .skip
-        mov rax, [data + 0x10]
-        push rax
+        push r14
         jmp loop
 .skip:  mov rcx, 1
 .sloop: call next
@@ -46,19 +46,19 @@ start:  mov rax, [data + 8]
         loop .sloop
         jmp loop
 end:    pop rsi
-        mov rax, [data + 8]
+        mov rax, r13
         mov al, [rax]
         test al, al
         jz loop
         push rsi
         dec rsi
-        mov [data + 0x10], rsi
+        mov r14, rsi
         mov rax, 8
-        mov rdi, [data]
+        mov rdi, r12
         xor rdx, rdx
         syscall
         jmp loop
-out:    mov rsi, [data + 8]
+out:    mov rsi, r13
         lea rdi, [rsp - 1]
         movsb
 in:     sub rax, 44
@@ -69,11 +69,10 @@ in:     sub rax, 44
         syscall
         test di, di
         jnz loop
-        mov rdi, [data + 8]
+        mov rdi, r13
         movsb
         jmp loop
 exit:   mov rax, 60
         xor rdi, rdi
         syscall
-        section .data
-        data dq 3 dup(0), exit, 42 dup(loop), id, in, id, out, 13 dup(loop), lr, loop, lr, 28 dup(loop), start, loop, end, 34 dup(loop)
+data:   dq exit, 42 dup(loop), id, in, id, out, 13 dup(loop), lr, loop, lr, 28 dup(loop), start, loop, end, 34 dup(loop)
